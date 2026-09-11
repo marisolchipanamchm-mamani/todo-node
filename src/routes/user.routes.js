aa5a5d40064febfb3de11cb15ae3780218c04cdc
marisol cbhipana <marisolchipana.mchm@gmail.com>
2026-09-10 22:13:20 -0400

    TAR-8 Implementar middleware JWT

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,9 +5,18 @@ const {
     loginUser
 } = require('../controllers/user.controller');
 
+const authMiddleware = require('../middlewares/auth.middleware');
+
 const router = express.Router();
 
 router.post('/', createUser);
 router.post('/login', loginUser);
+
+router.get('/perfil', authMiddleware, (req, res) => {
+    res.json({
+        mensaje: 'Acceso autorizado',
+        user: req.user
+    });
+});
 
 module.exports = router;
